b6309982847fc88b1ce85bece18b463c54137968
Igor Pudovkin <iggpd62@yandex.ru>
2023-06-05 18:24:39 +0300

    first commit

--- functions.py
+++ functions.py
@@ -1,30 +1,60 @@
-def show_data() -> None:
+def show_data():
     """Выводит информацию из справочника"""
     with open('book.txt', 'r', encoding='utf-8') as file:
         print(file.read())
 
 
-def add_data() -> None:
+def add_data():
     """Добавляет информацию в справочник."""
     fio = input('Введите ФИО: ')
     phone = input('Введите номер телефона: ')
     with open('book.txt', 'a', encoding='utf-8') as file:
-        file.write(f'\n{fio} | {phone}')
+        file.write(f'{fio} | {phone}\n')
 
 
-def find_data() -> None:
+def find_data():
     """Печатает результат поиска по справочнику."""
     with open('book.txt', 'r', encoding='utf-8') as file:
-        data = file.read()
-    print(data)
+        data = file.read().split('\n')
+    print('\n'.join(data))
     data_to_find = input('Введите данные для поиска: ')
     print(search(data, data_to_find))
 
 
-def search(book: str, info: str) -> str:
+def search(book, info):
     """Находит в списке записи по определенному критерию поиска"""
-    book = book.split('\n')
-    for contact in book:
-        if info in contact:
-            return contact
-    return 'Совпадений не найдено'
+    result = [contact for contact in book if info in contact]
+    if not result:
+        return 'Совпадений не найдено'
+    elif len(result) == 1:
+        return result[0]
+    elif len(result) > 1:
+        print()
+        print('---------------')
+        print('\n'.join(result))
+        new_info = input('Введите данные для уточнения: ')
+        return search(result, new_info)
+
+
+def change():
+    """Изменение/удаление данных в справочнике."""
+    with open('book.txt', 'r', encoding='utf-8') as file:
+        data = file.read().split('\n')
+    print('\n'.join(data))
+    data_to_edit = input('Введите данные для поиска: ')
+    data_to_edit = search(data, data_to_edit)
+    mode = input('Удалить или изменить? 1 - удалить, 2 - заменить')
+    if mode == '1':
+        data.remove(data_to_edit)
+    elif mode == '2':
+        new_contact = enter_contact()
+        data[data.index(data_to_edit)] = new_contact
+
+    with open('book.txt', 'w', encoding='utf-8') as file:
+        file.write('\n'.join(data))
+
+
+def enter_contact():
+    fio = input('Введите ФИО: ')
+    phone = input('Введите номер телефона: ')
+    return f'{fio} | {phone}'
